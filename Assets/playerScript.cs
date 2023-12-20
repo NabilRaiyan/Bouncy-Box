3d67ebd76f1cd158cf32b895438015c05344069e
Raiyan Al Sultan <raiyanalsultan@Raiyans-MacBook-Pro.local>
2023-12-20 20:07:16 +0600

    Adding player movement

---
+++ Assets/playerScript.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2200f;
+    public float sideForce = 500f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Time.deltatime means the time interval in seconds it took from the last frame to the current frame.
+        // It measures the interval between the current frame and the last one
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // adding key event listener to left or right movement of player
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
