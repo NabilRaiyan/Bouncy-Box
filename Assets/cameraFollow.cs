2ad4fd42030cde4f830454fc9709a2f8fa100ca5
NabilRaiyan <rraiyan77@gmail.com>
2023-12-21 18:28:35 +0600

    Adding obstacle

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -5,7 +5,7 @@ public class cameraFollow : MonoBehaviour
 {
 
     public Transform player;
-    public Vector3 offset; 
+    public Vector3 offset; // vector3 is storing 3 floting point number
 
     // Update is called once per frame
     void Update()
