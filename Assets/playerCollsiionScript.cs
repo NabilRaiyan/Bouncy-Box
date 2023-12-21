f24356257c852ab4961a8d81c9fe4501e880281c
NabilRaiyan <rraiyan77@gmail.com>
2023-12-21 18:53:11 +0600

    Adding Collsion

--- Assets/playerCollsiionScript.cs
+++ Assets/playerCollsiionScript.cs
@@ -5,6 +5,7 @@ public class playerCollsiionScript : MonoBehaviour
     // Checking collision with obstacle
     private void OnCollisionEnter(Collision collisionInfo)
     {
+        // checking collision with obstacle
         if (collisionInfo.transform.name == "obstacle")
         {
             Debug.Log("Hit");
