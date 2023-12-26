4b1d17332eca0a85837fc992d8058aec155630ea
NabilRaiyan <rraiyan77@gmail.com>
2023-12-26 19:17:54 +0600

    Adding score

---
+++ Assets/script/Score.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform playerPosition;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition.position.z;
+    }
+}
